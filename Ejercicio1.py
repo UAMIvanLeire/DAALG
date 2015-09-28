41ceb09e3df3f099335723fe06fc34940ff2f0ac
Leire <tercergen@gmail.com>
2015-09-29 00:46:57 +0200

    Hasta 2.2

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,3 +1,5 @@
+from numpy import *
+
 def initS():
  return []
 
@@ -31,12 +33,49 @@ def isPalyndrome(cadena):
  return True
 
 def isPalyndromeSmall(cadena):
- if cadena [len(cadena)-1::-1] == cadena:
-  return True
- return False
+ return cadena [len(cadena)-1::-1] == cadena
+
+def randomString(long, strBase):
+  list = initS();
+  for i in range(long):
+    push(random.choice(strBase), list)
+  return list
+
+def randomPalyndrome(long, strBase):
+  if (long%2) == 0:
+    list1 = randomString(long / 2, strBase)
+    list2 = list1 [len(list1)-1::-1]
+    return list1 + list2
+  else:
+    list1 = randomString(long / 2, strBase)
+    list2 = list1 [len(list1)-1::-1]
+    list3 = randomString(1, strBase)
+    return list1 + list3 + list2
+
+def generateRandomPalyndrList(numSt, longMax, probPalyndrome):
+  return 0
+
+def countPalyndromesList(l):
+  laux = l
+  list = initS()
+  for i in range(len(l)):
+    eleml = pop(l)
+    if isPalyndromeSmall(eleml):
+      push(eleml, list)
+  l = laux
+  return len(list)
+
+def list2file(l, fName):
+  f = open(fName, "w")
+  print len(l)
+  for i in range(len(l)):
+    f.write(pop(l))
+    f.write("\n")
+  f.close()
+  return
 
 
-#Pruebas funciones básicas
+#Pruebas funciones basicas
 list = initS()
 print emptyS(list)
 push(1, list)
@@ -64,5 +103,25 @@ print isPalyndrome("hola")
 print isPalyndromeSmall("hello")
 print isPalyndromeSmall("helloolleh")
 
+#Pruebas randomString
+print "Prueba randomString con cadena \"pali\""
+lista = ['p', 'a', 'l', 'i']
+print randomString(4, lista)
+print len(randomString(4,lista))
+print randomString(10, lista)
+
+#Pruebas randomPalyndrome
+print "Prueba randomPalyndrome con cadena \"pali\" y long impar"
+print randomPalyndrome(5, lista)
+print "Prueba randomPalyndrome con cadena \"pali\" y long par"
+print randomPalyndrome(6, lista)
+
+#Pruebas countPalyndromesList
+print "Prueba countPalyndromesList"
+list = ["hola", "hooh", "lola", "ollo", "miim"]
+print  countPalyndromesList(list)
 
 
+#Pruebas list2file
+print "Prueba list2file en archivo practica 1"
+list2file(list, "practica.txt")
